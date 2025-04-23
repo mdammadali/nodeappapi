8d2d30172804aa5e64e947e89ebd7914fd392c3b
Md Ammad Ali <mdammad75@gmail.com>
2025-04-23 11:33:39 +0600

    file update

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,21 @@ import { fileURLToPath } from 'url';
 
 dotenv.config({ path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../.env') });
 
+const env = process.env.NODE_ENV || 'development';
+const devConfig = {
+    mongodbUri: 'mongodb://localhost:27017/mern',
+}
+const baseConfig = {
+    port: process.env.PORT || 3000,
+    mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/mern',
+};
+const prodConfig = {
+    mongodbUri: process.env.MONGODB_URI,
+}
+console.log(process.env.MONGODB_URI);
+
 const config = {
-    port: process.env.PORT,
-    mongodbUri: process.env.mongodbUri,
+    ...baseConfig,
+    ...(env === 'development' ? devConfig : prodConfig),
 }
 export default config;
